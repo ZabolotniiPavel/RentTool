c97d6574203fe9e6727d31e9168dda3d64c3a7f0
zabolotniipavel <zabolotniipavel@gmail.com>
2022-09-17 19:18:53 +0300

    add initial app

---
+++ src/main/java/com/gmail/zabolotniipavel/renttools/RentToolsApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.zabolotniipavel.renttools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/zabolotniipavel/renttools/RentToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.zabolotniipavel.renttools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
