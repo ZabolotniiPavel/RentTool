1d6857477b43e335a9ce92013d8f631c44f7983a
zabolotniipavel <zabolotniipavel@gmail.com>
2022-09-18 18:19:05 +0300

    Adding internalization support

--- src/main/java/com/gmail/zabolotniipavel/renttools/RentConfig.java
+++ src/main/java/com/gmail/zabolotniipavel/renttools/RentConfig.java
@@ -2,10 +2,16 @@ package com.gmail.zabolotniipavel.renttools;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class RentConfig implements WebMvcConfigurer {
@@ -13,6 +19,23 @@ public class RentConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/img/**")
                 .addResourceLocations("/WEB-INF/img/");
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry){
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.forLanguageTag("ru-RU"));
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
     }
     @Bean
     public ViewResolver viewResolver(){
--- src/main/webapp/WEB-INF/jsp/postOffer.jsp
+++ src/main/webapp/WEB-INF/jsp/postOffer.jsp
@@ -1,5 +1,6 @@
 <%@ page contentType="text/html; charset=UTF-8" language="java" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
  <html lang="en">
  <head>
      <meta charset="UTF-8">
@@ -11,7 +12,7 @@
         <table>
             <tr>
                 <td>
-                    Item name:
+                    <spring:message code="name"/>
                 </td>
                 <td>
                     <form:input path="name" />
