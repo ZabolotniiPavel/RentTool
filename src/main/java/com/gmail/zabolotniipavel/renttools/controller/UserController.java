7040fd91ed6d750d25eb1f72bc3bc5883369c2cc
zabolotniipavel <zabolotniipavel@gmail.com>
2022-09-18 22:14:22 +0300

    adding sample rest controller

---
+++ src/main/java/com/gmail/zabolotniipavel/renttools/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.gmail.zabolotniipavel.renttools.controller;
+
+import com.gmail.zabolotniipavel.renttools.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    public User getUser(@Valid @RequestParam(value = "name", defaultValue = "Vadik Krasava") String name,
+                        @Valid @RequestParam(value = "mobile", defaultValue = "88005553555") String mobile){
+        User user = new User();
+        user.setUserName(name);
+        user.setMobileNumber(mobile);
+        return user;
+    }
+}
