f188bc52e7e868bff465fe243894de74a666b86e
zabolotniipavel <zabolotniipavel@gmail.com>
2022-09-19 22:12:27 +0300

    Adding User list of posts

--- src/main/java/com/gmail/zabolotniipavel/renttools/model/User.java
+++ src/main/java/com/gmail/zabolotniipavel/renttools/model/User.java
@@ -1,12 +1,15 @@
 package com.gmail.zabolotniipavel.renttools.model;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 public class User {
     @NotEmpty
     private String userName;
     @NotEmpty
     private String mobileNumber;
+
+    private List<Offer> userPosts;
 
     public String getUserName() {
         return userName;
@@ -23,4 +26,12 @@ public class User {
     public void setMobileNumber(String mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
+    public List<Offer> getUserPosts() {
+        return userPosts;
+    }
+
+    public void setUserPosts(List<Offer> userPosts) {
+        this.userPosts = userPosts;
+    }
+
 }
