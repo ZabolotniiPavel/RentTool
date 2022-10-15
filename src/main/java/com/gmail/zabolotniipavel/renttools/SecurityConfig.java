194d9240480d4e0c12b10c373ccb48107492418a
zabolotniipavel <zabolotniipavel@gmail.com>
2022-10-15 21:24:53 +0300

    Adding in database authentication

--- src/main/java/com/gmail/zabolotniipavel/renttools/SecurityConfig.java
+++ src/main/java/com/gmail/zabolotniipavel/renttools/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.gmail.zabolotniipavel.renttools;
 
+import com.gmail.zabolotniipavel.renttools.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
+
+    @Autowired
+    CustomUserDetailService customUserDetailService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -27,16 +35,5 @@ public class SecurityConfig  {
                 .logout((logout) -> logout.logoutSuccessUrl("/welcome").invalidateHttpSession(true).permitAll()   );
 
         return http.build();
-    }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
