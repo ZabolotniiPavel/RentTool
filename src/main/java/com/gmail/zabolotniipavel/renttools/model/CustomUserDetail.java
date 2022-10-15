b7e09a84703569f06c4f88878b3438354b455355
zabolotniipavel <zabolotniipavel@gmail.com>
2022-10-15 21:07:17 +0300

    Adding Custom UserrDetail Implementation

---
+++ src/main/java/com/gmail/zabolotniipavel/renttools/model/CustomUserDetail.java
@@ -0,0 +1,49 @@
+package com.gmail.zabolotniipavel.renttools.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private final User user;
+    public CustomUserDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "read");
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
